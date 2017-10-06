1f7f2126148b6f23eda9df4b370718582301f0b6
santi-2017 <ballesteross975@gmail.com>
2017-10-06 14:28:13 -0500

    ingreso de javascript a esta pagina
    pregunta de la edad de la persona

---
+++ js/linea.js
@@ -0,0 +1,8 @@
+var edad=prompt("eres mayor de edad? ingresa tu edad");
+
+if(edad>=18){
+    alert("eres bienvenido a nuestra pagina");
+}
+else{
+    alert("no eres mayor de edad aque vienes? escribenos");
+}
