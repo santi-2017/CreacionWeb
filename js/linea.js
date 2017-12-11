ec6cc1d6603a004a3c90788af239d217cccb0912
santi-2017 <30737241+santi-2017@users.noreply.github.com>
2017-12-11 12:50:18 -0500

    cambio de liena javascript

--- js/linea.js
+++
@@ -1,8 +0,0 @@
-var edad=prompt("eres mayor de edad? ingresa tu edad");
-
-if(edad>=18){
-    alert("eres bienvenido a nuestra pagina");
-}
-else{
-    alert("no eres mayor de edad aque vienes? escribenos");
-}
